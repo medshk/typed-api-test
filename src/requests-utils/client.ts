e1db5fae6b9cded6b5f03cf31d5baef1313650a4
med <mohammed.bennaceur98@gmail.com>
2024-01-05 09:15:13 +0100

    change the code to return the returnType of T arg instead of T

--- src/requests-utils/client.ts
+++ src/requests-utils/client.ts
@@ -7,12 +7,12 @@ import { Err, Result } from './result.types';
 type Payload = Record<string, string>;
 
 export const makeClientForEndpoint = <T extends (...args: any[]) => any>(ep: Endpoint<T>) => ({
-  fire: async (payload: Payload): Promise<T | Err> => {
+  fire: async (payload: Payload): Promise<ReturnType<T> | Err> => {
     // TODO. You can use axios which is already installed.
     // Let's say all endpoints use POST method for now.
     try {
       const response: AxiosResponse = await axios.post(baseURL + ep.path, payload);
-      return response.data as T;
+      return response.data as ReturnType<T>;
     } catch (error) {
       return error as Err;
     }
